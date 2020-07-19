bc1839bfdb3e9c5f7dafb163c1c7dee8a445cd05
Lucas <lucas.tnagel@gmail.com>
2020-07-19 16:51:22 -0300

    Adding sky

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as THREE from "../three.js/build/three.module.js";
 import {OrbitControls} from "../three.js/examples/jsm/controls/OrbitControls.js";
 import {GLTFLoader} from "../three.js/examples/jsm/loaders/GLTFLoader.js";
 import Clouds from "./clouds.js";
+import { Sky } from "../three.js/examples/jsm/objects/Sky.js";
 import * as dat from "../dat.gui/build/dat.gui.module.js";
 
 let scene = new THREE.Scene();
@@ -13,7 +14,6 @@ renderer.setClearColor(0x787e74, 1);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
-
 
 let light = new THREE.PointLight(0xffffff);
 light.position.set(0, 20, 10);
@@ -36,15 +36,58 @@ camera.position.z = 15;
 
 controls.update();
 
-const panel = new dat.GUI({autoPlace: true});
-panel.domElement.id = 'gui';
-let folder = panel.addFolder('Sun');
-let params = {
-    'Position': 0
+let sky = new Sky();
+sky.scale.setScalar(45000);
+scene.add(sky);
+
+let sun = new THREE.Vector3();
+let effectController = {
+    turbidity: 10,
+    rayleigh: 3,
+    mieCoefficient: 0.005,
+    mieDirectionalG: 0.7,
+    inclination: 0.49, // elevation / inclination
+    azimuth: 0.25, // Facing front,
+    exposure: renderer.toneMappingExposure
+};
+
+
+function guiChanged() {
+
+    var uniforms = sky.material.uniforms;
+    uniforms[ "turbidity" ].value = effectController.turbidity;
+    uniforms[ "rayleigh" ].value = effectController.rayleigh;
+    uniforms[ "mieCoefficient" ].value = effectController.mieCoefficient;
+    uniforms[ "mieDirectionalG" ].value = effectController.mieDirectionalG;
+
+    var theta = Math.PI * ( effectController.inclination - 0.5 );
+    var phi = 2 * Math.PI * ( effectController.azimuth - 0.5 );
+
+    sun.x = Math.cos( phi );
+    sun.y = Math.sin( phi ) * Math.sin( theta );
+    sun.z = Math.sin( phi ) * Math.cos( theta );
+
+    uniforms[ "sunPosition" ].value.copy( sun );
+    light.position.x = sun.x*1000;
+    light.position.y = sun.y*1000;
+    light.position.z = sun.z*1000;
+
+    renderer.toneMappingExposure = effectController.exposure;
+    renderer.render( scene, camera );
+
 }
-folder.add(params, 'Position', -1, 1).step(0.01).onChange(function (value) {
-    console.log(value);
-});
+
+let gui = new dat.GUI();
+
+gui.add( effectController, "turbidity", 0.0, 20.0, 0.1 ).onChange( guiChanged );
+gui.add( effectController, "rayleigh", 0.0, 4, 0.001 ).onChange( guiChanged );
+gui.add( effectController, "mieCoefficient", 0.0, 0.1, 0.001 ).onChange( guiChanged );
+gui.add( effectController, "mieDirectionalG", 0.0, 1, 0.001 ).onChange( guiChanged );
+gui.add( effectController, "inclination", 0, 1, 0.0001 ).onChange( guiChanged );
+gui.add( effectController, "azimuth", 0, 1, 0.0001 ).onChange( guiChanged );
+gui.add( effectController, "exposure", 0, 1, 0.0001 ).onChange( guiChanged );
+
+guiChanged();
 
 var animate = function () {
     requestAnimationFrame( animate );
